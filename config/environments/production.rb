70c73207ce46154fadcb218bbfecb5ca6c4c50bf
Luca Peduto <l.peduto@sensible.it>
2011-07-10 16:04:47 +0200

    Moved ActionMailer config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Tasquee::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
