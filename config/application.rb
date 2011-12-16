e65ca69b95aaf931a7540a0983f1c4e8c518cdce
Luca Peduto <l.peduto@sensible.it>
2011-12-16 11:25:21 +0100

    Modified soma migrations

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,6 @@ module Tasquee
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    
-    ActiveRecord::ConnectionAdapters::MysqlAdapter.emulate_booleans = false
+
   end
 end
